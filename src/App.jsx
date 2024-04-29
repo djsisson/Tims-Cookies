aad90cb157c6b7cfb40d766248f421260e438a90
Darren Sisson <darrensisson@hotmail.com>
2024-04-29 21:43:38 +0100

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,35 @@ export default function App() {
     const myInterval = setInterval(() => {
       addCookie();
     }, 1000);
-    oldTotal = currentTotal
+
+    oldTotal = currentTotal; //set initial variable on first setup this is to avoid a catchup if using localstorage
     const displayInterval = setInterval(() => {
-      const diff = ((currentTotal-oldTotal)/4)
-      oldTotal += diff
-      setDisplayTotal(Math.floor(oldTotal))
+      oldTotal =
+        currentTotal < oldTotal  //check if we have spent 
+          ? currentTotal // if we have jump to current total
+          : oldTotal + Math.ceil((currentTotal - oldTotal) / 4); // if not move 25% of the difference 
+          /*Note if the difference is 100, this equates to on each tick of (rounding up):
+          1) (100/4) = 25 :- 75 remaining
+          2) (75/4) = 19 :- 56 remaining
+          3) (56/4) = 14 :- 42 remaining
+          4) (42/4) = 11 :- 31 remaining
+          5) (31/4) = 8 :- 23 remaining
+          6) (23/4) = 6 :- 17 remaining
+          7 (17/4) = 5 :- 12 remaining
+          8 (12/4) = 3 :- 9 remaining
+          9 (9/4) = 3 :- 6 remaining
+          10 (6/4) = 2 : 4 remaining
+          Next tick happens here if you look at difference on page at 100cps you will see it briefly say 104
+          as you can see we never actually get to the total before the next cps ticks up but we get close
+
+          */
+      setDisplayTotal(Math.ceil(oldTotal)); //updated state variable to display the calculated variable
     }, 100);
 
     // to clean up my timer when I rerun the useEffect to i don't end up with a billion timers
     return () => {
       clearInterval(myInterval);
-      clearInterval(displayInterval)
+      clearInterval(displayInterval);
     };
   }, []);
 
@@ -47,7 +65,7 @@ export default function App() {
   }
 
   function buyUpgrade() {
-    setCps((cps) => (cps+1));
+    setCps((cps) => cps + 1);
   }
 
   return (
@@ -57,7 +75,7 @@ export default function App() {
       <button onClick={buyUpgrade}>Buy upgrade</button>
       <p>I have {displayTotal} cookies</p>
       <p>I get {cps} cookies per second</p>
-      <p>difference of {cookies-displayTotal}</p>
+      <p>difference of {cookies - displayTotal}</p>
     </div>
   );
 }
