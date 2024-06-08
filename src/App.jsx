8d0a46c5eb7a9186271987f99accac66436c3d9f
Darren Sisson <darrensisson@hotmail.com>
2024-06-08 13:47:19 +0100

    switch timers to useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,51 @@
-import { useState, useEffect } from "react";
-
-let oldTotal = 0;
-let currentTotal = 0;
-let currentCPS = 0;
+import { useState, useEffect, useRef } from "react";
 
 export default function App() {
   // what if I want to do local storage? STRETCH GOAL
   const [cookies, setCookies] = useState(0);
   const [cps, setCps] = useState(1); // CPS = Cookies Per Second
   const [displayTotal, setDisplayTotal] = useState(0);
+  const refAddCookie = useRef();
+  const refUpdateTotal = useRef();
 
   useEffect(() => {
-    currentCPS = cps;
-  }, [cps]);
-
-  useEffect(() => {
-    currentTotal = cookies;
-  }, [cookies]);
+    refAddCookie.current = addCookie;
+    refUpdateTotal.current = updateTotal;
+  });
 
   useEffect(() => {
     // maybe you want to do some maths here for the 1000/cps etc
     // a timer to be created when the page loads to increase cookies by cps every second
     const myInterval = setInterval(() => {
-      addCookie();
+      refAddCookie.current();
     }, 1000);
 
-    oldTotal = currentTotal; //set initial variable on first setup this is to avoid a catchup if using localstorage
+    // oldTotal = currentTotal; //set initial variable on first setup this is to avoid a catchup if using localstorage
     const displayInterval = setInterval(() => {
-      oldTotal =
-        currentTotal < oldTotal  //check if we have spent 
-          ? currentTotal // if we have jump to current total
-          : oldTotal + Math.ceil((currentTotal - oldTotal) / 4); // if not move 25% of the difference 
-          /*Note if the difference is 100, this equates to on each tick of (rounding up):
+      refUpdateTotal.current();
+    }, 100);
+
+    // to clean up my timer when I rerun the useEffect to i don't end up with a billion timers
+    return () => {
+      clearInterval(myInterval);
+      clearInterval(displayInterval);
+    };
+  }, []);
+
+  function addCookie() {
+    // because this runs in a timer, we need to be more explicit about the previous value of the state variable
+    setCookies((currentCookies) => {
+      // what if I want to do local storage? STRETCH GOAL
+      return currentCookies + cps;
+    });
+  }
+
+  function updateTotal() {
+    const updatedTotal =
+      cookies < displayTotal //check if we have spent
+        ? cookies // if we have jump to current total
+        : displayTotal + Math.ceil((cookies - displayTotal) / 4); // if not move 25% of the difference
+    /*Note if the difference is 100, this equates to on each tick of (rounding up):
           1) (100/4) = 25 :- 75 remaining
           2) (75/4) = 19 :- 56 remaining
           3) (56/4) = 14 :- 42 remaining
@@ -46,22 +60,7 @@ export default function App() {
           as you can see we never actually get to the total before the next cps ticks up but we get close
 
           */
-      setDisplayTotal(Math.ceil(oldTotal)); //updated state variable to display the calculated variable
-    }, 100);
-
-    // to clean up my timer when I rerun the useEffect to i don't end up with a billion timers
-    return () => {
-      clearInterval(myInterval);
-      clearInterval(displayInterval);
-    };
-  }, []);
-
-  function addCookie() {
-    // because this runs in a timer, we need to be more explicit about the previous value of the state variable
-    setCookies((currentCookies) => {
-      // what if I want to do local storage? STRETCH GOAL
-      return currentCookies + currentCPS;
-    });
+    setDisplayTotal(Math.ceil(updatedTotal)); //updated state variable to display the calculated variable
   }
 
   function buyUpgrade() {
